afcff8d500d7f9bc2827dd0061082391332c76e4
Raniere Soares <ranierepsa@gmail.com>
2020-09-15 20:49:29 -0300

    Adding a controller for the paddler
    Adding a controller for the paddler

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    [SerializeField] float minX = -7f;
+    [SerializeField] float maxX = 7f;
+    [SerializeField] float screenWidthInUnits = 16;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = (Input.mousePosition.x / Screen.width * screenWidthInUnits) - screenWidthInUnits / 2;
+        transform.position = new Vector2(Mathf.Clamp(xPos, minX, maxX) , transform.position.y);
+    }
+}
